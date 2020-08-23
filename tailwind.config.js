ed15f197d24dc6ec2e21a9ff20ac637265f1a243
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-08-23 14:58:52 +0000

    Added Tailwind config files, Added link to tailwindcss output in index.ejs

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// This is the config file for tailwindcss https://tailwindcss.com/docs/configuration
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
