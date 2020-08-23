184469562b09cd030621ddb336adef648d6b54c5
micmor-m <micmor.m@gmail.com>
2020-08-23 15:14:15 +0000

    create tables

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,12 @@
--- Drop and recreate Users table (Example)
+-- Drop and recreate Users table
 
 DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) NOT NULL,
+  is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
