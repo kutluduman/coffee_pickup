8c1b124eae650329a6a61eefdfcf82856a235767
micmor-m <micmor.m@gmail.com>
2020-08-24 13:04:20 +0000

    query fro check_out and add few more seeds for test

--- db/seeds/07_ options_items.sql
+++ db/seeds/07_ options_items.sql
@@ -4,3 +4,5 @@
 INSERT INTO options_items (option_id, order_item_id, price) VALUES (1, 5, 33);
 -- qty 1 cream for capuccino
 INSERT INTO options_items (option_id, order_item_id, price) VALUES (1, 4, 66);
+-- more seeds
+INSERT INTO options_items (option_id, order_item_id, price) VALUES (5, 10, 33);
