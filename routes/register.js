9d4438236a5b81304f239bc37ad40462dbf20a4d
micmor-m <micmor.m@gmail.com>
2020-08-24 19:12:45 +0000

    pull request from master

--- routes/register.js
+++ routes/register.js
@@ -22,44 +22,85 @@ module.exports = (db) => {
     res.render("register");
   });
 
+  //TO DO:
+    // Make function userExists(arg1, arg2)
+    // runs a query checking for those values if they exist in users table
 
-  router.post("/", (req, res) => {
-    const text =
-      "INSERT INTO users (name, email, password, phone, is_admin) VALUES($1, $2, $3, $4, $5) RETURNING *";
-    const values = [req.body.name, req.body.email, req.body.password, req.body.phone, false];
-    db.query(text, values)
-      .then((res) => {
-        console.log(res.rows[0]);
-        console.log("Email",res.rows[0].email);
-        const text1 =
+    const userExists =(email, phone) =>{
+      const text =
         `SELECT id, name, email, password, phone, is_admin
         FROM users
-        WHERE phone = $1 AND email = $2;`;
-        const values1 = [res.rows[0].phone, res.rows[0].email];
-        db.query(text1, values1)
+        WHERE email = $1 AND phone = $2;`;
+        const values = [email, phone];
+        db.query(text, values)
           .then((result) => {
-            console.log("Result from query find user by email", ressult.rows[0]);
-        //  const user = findUserByEmail(email);
+            console.log("Result from query find user by email", result.rows[0]);
+            if ((result.email === email) & (result.phone === phone)) {
+             return result.rows[0];
+            }
+          })
 
-        // if (!user) {
-        //   const userId = addNewUser(name, email, password);
-        //   // setCookie with the user id
-        //   req.session['user_id'] = userId;
+      // after the loop, return false
+      return false;
+    }
 
-        //   // redirect to /quotes
-        //   res.redirect('/quotes');
-        // } else {
-        //   res.status(403).send('Sorry, the user is already registered');
-        // }
-      })
+  router.post('/', (req, res)=>{
+    let user = userExists(req.body.email, req.body.phone);
+    console.log("user: ", user)
+      if (user) {
+      console.log("User already present", user)
+      //return error message user exits use different email or phone
+    } else {
+      const text = "INSERT INTO users (name, email, password, phone, is_admin) VALUES($1, $2, $3, $4, $5) RETURNING *";
+      const values = [req.body.name, req.body.email, req.body.password, req.body.phone, false];
+      db.query(text, values)
+        .then(dbRes => {
+          res.redirect('/menu')
+        })
+        .catch(err => {
+          console.log('Something Broke !', err);
+        })
+    }
+  })
 
-        res.redirect('/menu');
 
-        // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
-      })
-      .catch((e) => console.error(e.stack));
+  // router.post("/", (req, res) => {
+  //   const text =
+  //     "INSERT INTO users (name, email, password, phone, is_admin) VALUES($1, $2, $3, $4, $5) RETURNING *";
+  //   const values = [req.body.name, req.body.email, req.body.password, req.body.phone, false];
+  //   db.query(text, values)
+  //     .then((res) => {
+  //       console.log(res.rows[0]);
+  //       console.log("Email",res.rows[0].email);
+  //       const text1 =
+  //       `SELECT id, name, email, password, phone, is_admin
+  //       FROM users
+  //       WHERE phone = $1 AND email = $2;`;
+  //       const values1 = [res.rows[0].phone, res.rows[0].email];
+  //       db.query(text1, values1)
+  //         .then((result) => {
+  //           console.log("Result from query find user by email", ressult.rows[0]);
+  //       //  const user = findUserByEmail(email);
 
-  });
+  //       // if (!user) {
+  //       //   const userId = addNewUser(name, email, password);
+  //       //   // setCookie with the user id
+  //       //   req.session['user_id'] = userId;
+
+  //       //   // redirect to /quotes
+  //       //   res.redirect('/quotes');
+  //       // } else {
+  //       //   res.status(403).send('Sorry, the user is already registered');
+  //       // }
+  //     })
+
+  //       res.redirect('/menu');
+
+  //       // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+  //     })
+  //     .catch((e) => console.error(e.stack));
+
+  // });
 
   return router;
 };
