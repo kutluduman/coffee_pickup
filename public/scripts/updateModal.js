af651c17f1a671e8632d6b39af4b44009cb79755
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-08-25 21:57:04 +0000

    Made modal dynamic based off item category

--- public/scripts/updateModal.js
+++ public/scripts/updateModal.js
@@ -1,11 +1,14 @@
-const updateModal = (data) => {
-  const cat = data;
-  console.log(cat);
-};
-
+const optionsContainer = document.getElementById('options-container');
 const menuItems = document.getElementsByClassName("menu-item-card");
 Array.from(menuItems).forEach((menuItem) => {
   menuItem.addEventListener("click", function () {
-    console.log(this.getAttribute('category'));
+    const category = this.getAttribute("category");
+    if (category === "Coffee") {
+      optionsContainer.classList.remove('hidden')
+      optionsContainer.classList.add('block')
+    } else if (category === "Bakery") {
+      optionsContainer.classList.remove('block')
+      optionsContainer.classList.add('hidden')
+    }
   });
 });
