5763855238ebd1333a6f6731edbf44a826e6e30e
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-08-27 02:18:37 +0000

    Finished admin ui, optimizations made to the admin car dui

---
+++ routes/adminDash.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  const items = () => {
+    const text = `
+      SELECT orders.id, users.name as name
+      FROM orders
+      JOIN users ON users.id = user_id
+      WHERE pickup_ready = true
+      `;
+   
+    return db.query(text).then((result) => {
+      if (result.rows !== undefined) {
+        console.log("Result from query items", result.rows);
+        if (result.rows) {
+          return result.rows;
+        }
+      } else {
+        return false;
+      }
+    });
+  };
+
+
+  router.get("/", (req, res) => {
+    items().then((items) => {
+      
+        let templateVars = { orders: items };
+        res.render("admin_dash", templateVars);
+      
+    });
+  });
+
+  return router;
+};
