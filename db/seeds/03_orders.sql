faebb1073a98e7e7fd611c2d591e4ae436c8f74d
micmor-m <micmor.m@gmail.com>
2020-08-27 02:25:14 +0000

    worked on sms api and sending datafrom cart to database

--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -1,8 +1,8 @@
 -- orders table seeds here
-INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (2, FALSE, '2020-08-12T08:00:00.000Z', FALSE);
-INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (3, FALSE, '2020-08-16T08:00:00.000Z', FALSE);
-INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (1, TRUE, '2020-08-23T08:00:00.000Z', FALSE);
-INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (4, TRUE, '2020-08-23T09:30:00.000Z', FALSE);
-INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (2, FALSE, '2020-08-23T09:30:00.000Z', TRUE);
-INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (3, FALSE, '2020-08-15T08:00:00.000Z', FALSE);
-INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (3, FALSE, '2020-08-14T08:00:00.000Z', FALSE);
+INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (2, FALSE, NOW()::timestamp, FALSE);
+INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (3, FALSE, NOW()::timestamp, FALSE);
+INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (1, TRUE, NOW()::timestamp, FALSE);
+INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (4, TRUE, NOW()::timestamp, FALSE);
+INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (2, FALSE, NOW()::timestamp, TRUE);
+INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (3, FALSE, NOW()::timestamp, FALSE);
+INSERT INTO orders (user_id, in_progress, time_ordered, pickup_ready) VALUES (3, FALSE, NOW()::timestamp, FALSE);
