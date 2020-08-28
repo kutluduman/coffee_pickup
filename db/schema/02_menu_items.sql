df9b0896938db1cf256c0617652fdc1569678ec6
micmor-m <micmor.m@gmail.com>
2020-08-28 14:30:35 +0000

    update pictures in table

--- db/schema/02_menu_items.sql
+++ db/schema/02_menu_items.sql
@@ -6,7 +6,7 @@ CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   price INTEGER  NOT NULL DEFAULT 0,
-  picture_url VARCHAR(255) NOT NULL,
+  picture_url TEXT NOT NULL,
   prep_time INTEGER  NOT NULL DEFAULT 0,
   description TEXT NOT NULL,
   in_stock BOOLEAN NOT NULL DEFAULT TRUE,
